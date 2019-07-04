1d2e53322cf32de0afd2ca6227439198597674c7
Edward C. Deaver, IV <EdwardDeaver@users.noreply.github.com>
2019-07-04 06:07:45 -0400

    back

--- JS/scripts.js
+++ JS/scripts.js
@@ -136,7 +136,7 @@ var start = 0;
         // URL: String type
         // Type: ASYNC
         async function getData(url) {
-            const response = await fetch(url);
+            const response = await fetch(url); 
             const data = await response.text();
 
             return data;
